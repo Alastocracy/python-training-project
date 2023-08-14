f9dd6a94420891c8971a368116a05a160b0574db
Alastair Rees <alastair_rees@yahoo.com>
2023-08-14 16:04:51 +0100

    add level and xp

--- start.py
+++ start.py
@@ -4,8 +4,11 @@ name = input("Hello Adventurer, what is your name? ")
 
 character = {
     "charname": name,
+    "lvl": 1,
+    "xp": 0,
     "hp": 20,
-    "dmg": 1
+    "dmg": 1,
+
 }
 
 print(character)
