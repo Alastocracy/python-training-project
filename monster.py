02a608321f6461be95d48c2837337e95391d5fe1
Alastair Rees <alastair_rees@yahoo.com>
2023-08-20 17:42:15 +0100

    add fight, time and start floors

--- monster.py
+++ monster.py
@@ -3,29 +3,29 @@ import random
 #pick a random low level monster
 class monster():
   monster_list = [
-    {"type": "giant rat", "hp": 10, "attack": "bites", "dmg": 1, "xp": 2},
-    {"type": "goblin", "hp": 10,"attack": "stabs", "dmg": 1, "xp": 10},
-    {"type": "orc", "hp": 15, "attack": "slashes","dmg": 2, "xp": 15},
-    {"type": "hobgoblin", "hp": 20, "attack": "smashes", "dmg": 2, "xp": 20}
+    {"type": "Giant Rat", "hp": 10, "attack": "bites", "dmg": 1, "xp": 2},
+    {"type": "Goblin", "hp": 10,"attack": "stabs", "dmg": 1, "xp": 10},
+    {"type": "Orc", "hp": 15, "attack": "slashes","dmg": 2, "xp": 15},
+    {"type": "Hobgoblin", "hp": 20, "attack": "smashes", "dmg": 2, "xp": 20}
     ]
   this_monster = random.choice(monster_list)
 
 #pick a random mid level monster
 class bigger_monster():
   monster_list = [
-    {"type": "hobgoblin", "hp": 20, "attack": "smashes", "dmg": 2, "xp": 20},
-    {"type": "swarm of rats", "hp": 40, "attack": "smashes", "dmg": 1, "xp": 20},
-    {"type": "wolf", "hp": 20, "attack": "bites", "dmg": 4, "xp": 40},
-    {"type": "skeletal swordsman", "hp": 40, "attack": "slashes", "dmg": 2, "xp": 40}
+    {"type": "Hobgoblin", "hp": 20, "attack": "smashes", "dmg": 2, "xp": 20},
+    {"type": "Swarm of Rats", "hp": 40, "attack": "smashes", "dmg": 1, "xp": 20},
+    {"type": "Wolf", "hp": 20, "attack": "bites", "dmg": 4, "xp": 40},
+    {"type": "Skeletal Swordsman", "hp": 40, "attack": "slashes", "dmg": 2, "xp": 40}
   ]
   this_monster = random.choice(monster_list)
 
 #pick a high level monster
 class biggest_monster():
   monster_list = [
-    {"type": "skeletal swordsman", "hp": 40, "attack": "slashes", "dmg": 2, "xp": 40},
-    {"type": "death knight", "hp": 60, "attack": "strikes", "dmg": 5, "xp": 60},
-    {"type": "ghoul", "hp": 60, "attack": "curses", "dmg": 1, "xp": 50},
-    {"type": "warlock", "hp": 30, "attack": "zaps", "dmg": 10, "xp": 60},
+    {"type": "Skeletal Swordsman", "hp": 40, "attack": "slashes", "dmg": 2, "xp": 40},
+    {"type": "Death Knight", "hp": 60, "attack": "strikes", "dmg": 5, "xp": 60},
+    {"type": "Ghoul", "hp": 60, "attack": "curses", "dmg": 1, "xp": 50},
+    {"type": "Warlock", "hp": 30, "attack": "zaps", "dmg": 10, "xp": 60},
   ]
   this_monster = random.choice(monster_list)
--- start.py
+++ start.py
@@ -5,6 +5,9 @@ from monster import monster
 from monster import bigger_monster
 from monster import biggest_monster
 
+#Clear terminal at start
+os.system('cls')
+
 #Intro
 name = input("Hello Adventurer, what is your name? ")
 
@@ -13,12 +16,11 @@ character = {
     "charname": name,
     "lvl": 1,
     "xp": 0,
-    "hp": 20,
-    "dmg": 2,
+    "hp": 30,
+    "dmg": 3,
 }
 
 #Explain the game
-os.system('cls')
 time.sleep(2)
 print("Welcome " + character["charname"] + " to The Tower.")
 time.sleep(2)
@@ -30,7 +32,7 @@ os.system('cls')
 time.sleep(2)
 
 #Start lvl 1
-print("TOWER LEVEL 1")
+print("FLOOR 1")
 time.sleep(2)
 
 #encounter function to pick enemies based on char level
@@ -41,8 +43,39 @@ def encounter():
         enemy = bigger_monster()
     elif character["lvl"] < 7:
         enemy = biggest_monster()
-
+    
+    print("From the darkness a foe approaches...")
+    time.sleep(2)
     print("A " + enemy.this_monster["type"] + " appears!")
+
+    while enemy.this_monster["hp"] > 0 and character["hp"] > 0:
+        time.sleep(2)
+        print("The " + enemy.this_monster["type"] + " " + enemy.this_monster["attack"] + " you for " + str(enemy.this_monster["dmg"]) +" damage.")
+        enemy.this_monster["hp"] = enemy.this_monster["hp"] - character["dmg"]
+        time.sleep(2)
+        character["hp"] = character["hp"] - enemy.this_monster["dmg"]
+
+        print(character["charname"] + " hit points remaining: " + str(character["hp"]) + ", " + enemy.this_monster["type"] + " hit points remaining: " + str(enemy.this_monster["hp"]))
+
+    if enemy.this_monster["hp"] < 1:
+        time.sleep(1)
+        print("You have defeated the " + enemy.this_monster["type"] + "!")
+        xp_earned = enemy.this_monster["xp"]
+        character["xp"] = character["xp"] + xp_earned
+    else: 
+        time.sleep(1)
+        print("You have failed at level" + character["lvl"] + ", better luck next time!")
+        exit()
+
+    if character["xp"] >= (character["lvl"] * 20):
+        character["lvl"] = character["lvl"] + 1
+        character["xp"] = 0
+        print("Congratulations " + character["charname"] + " you have cleared this level, and may ascend up to Floor " + str(character["lvl"]) + "!")
+    else:
+        time.sleep(2)
+        print("You must defeat more enemies to advance.")
+        time.sleep(1)
+        encounter()
 
 encounter()
     
