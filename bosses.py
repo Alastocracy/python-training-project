25b7f17a40564ee0f161d1f8c40af591739f2394
Alastair Rees <alastair_rees@yahoo.com>
2023-08-20 23:23:18 +0100

    develop bosses

---
+++ bosses.py
@@ -0,0 +1,17 @@
+import random
+
+class dragon():
+    dragon = {"name": "", "type": "dragon", "hp": 20, "attack": "scorches", "dmg": 5}
+
+    name_list = ["Drakaroth", "Sylvestra", "Auronex", "Virelith", "Zephyrion", "Nivalis", "Seraphira", "Nocturnis", "Verdantix", "Celestrix", "Solarius", 
+                "Pyralis", "Tyranthor", "Lumidra", "Infernix", "Aquavox", "Jormungandr", "Nidhogg", "Fafnir", "Midgardsormr", "Lyngbakr", "Nafnafell", 
+                "Hafgufa", "Otr", "Fjalar", "Jǫrmungandr", "Elivagar"]
+
+    dragon["name"] = random.choice(name_list)
+
+class necromancer():
+    necromancer = {}
+
+
+class vampire():
+    vampire = {}
