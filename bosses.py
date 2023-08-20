0da08aa04214565346a41e19902fe607614f0fd2
Alastair Rees <alastair_rees@yahoo.com>
2023-08-20 23:37:58 +0100

    add details for dragon boss

--- bosses.py
+++ bosses.py
@@ -1,17 +1,15 @@
 import random
 
-class dragon():
-    dragon = {"name": "", "type": "dragon", "hp": 20, "attack": "scorches", "dmg": 5}
+class boss():
+    boss = {"name": "", "type": "", "hp": 0, "attack": "", "dmg": 0}
 
-    name_list = ["Drakaroth", "Sylvestra", "Auronex", "Virelith", "Zephyrion", "Nivalis", "Seraphira", "Nocturnis", "Verdantix", "Celestrix", "Solarius", 
+    boss_types = ["Dragon"]
+
+    boss["type"] = random.choice(boss_types)
+
+    if boss["type"] == "Dragon":
+        boss = {"name": "", "type": "Dragon", "hp": 20, "attack": "scorches", "dmg": 5}
+        dragon_names = ["Drakaroth", "Sylvestra", "Auronex", "Virelith", "Zephyrion", "Nivalis", "Seraphira", "Nocturnis", "Verdantix", "Celestrix", "Solarius", 
                 "Pyralis", "Tyranthor", "Lumidra", "Infernix", "Aquavox", "Jormungandr", "Nidhogg", "Fafnir", "Midgardsormr", "Lyngbakr", "Nafnafell", 
                 "Hafgufa", "Otr", "Fjalar", "Jǫrmungandr", "Elivagar"]
-
-    dragon["name"] = random.choice(name_list)
-
-class necromancer():
-    necromancer = {}
-
-
-class vampire():
-    vampire = {}
+        boss["name"] = random.choice(dragon_names)
