73083a44758e285a3b970e1d3cf881c475c94125
Alastair Rees <alastair_rees@yahoo.com>
2023-08-20 15:29:44 +0100

    add different levels of monsters

--- monster.py
+++ monster.py
@@ -1,5 +1,6 @@
 import random
 
+#pick a random low level monster
 class monster():
   monster_list = [
     {"type": "giant rat", "hp": 10, "attack": "bites", "dmg": 1, "xp": 2},
@@ -7,4 +8,24 @@ class monster():
     {"type": "orc", "hp": 15, "attack": "slashes","dmg": 2, "xp": 15},
     {"type": "hobgoblin", "hp": 20, "attack": "smashes", "dmg": 2, "xp": 20}
     ]
+  this_monster = random.choice(monster_list)
+
+#pick a random mid level monster
+class bigger_monster():
+  monster_list = [
+    {"type": "hobgoblin", "hp": 20, "attack": "smashes", "dmg": 2, "xp": 20},
+    {"type": "swarm of rats", "hp": 40, "attack": "smashes", "dmg": 1, "xp": 20},
+    {"type": "wolf", "hp": 20, "attack": "bites", "dmg": 4, "xp": 40},
+    {"type": "skeletal swordsman", "hp": 40, "attack": "slashes", "dmg": 2, "xp": 40}
+  ]
+  this_monster = random.choice(monster_list)
+
+#pick a high level monster
+class biggest_monster():
+  monster_list = [
+    {"type": "skeletal swordsman", "hp": 40, "attack": "slashes", "dmg": 2, "xp": 40},
+    {"type": "death knight", "hp": 60, "attack": "strikes", "dmg": 5, "xp": 60},
+    {"type": "ghoul", "hp": 60, "attack": "curses", "dmg": 1, "xp": 50},
+    {"type": "warlock", "hp": 30, "attack": "zaps", "dmg": 10, "xp": 60},
+  ]
   this_monster = random.choice(monster_list)
--- start.py
+++ start.py
@@ -1,8 +1,14 @@
 import random
+import time
+import os
 from monster import monster
+from monster import bigger_monster
+from monster import biggest_monster
 
+#Intro
 name = input("Hello Adventurer, what is your name? ")
 
+#Create starting character teplate
 character = {
     "charname": name,
     "lvl": 1,
@@ -11,11 +17,32 @@ character = {
     "dmg": 2,
 }
 
-print(character)
+#Explain the game
+os.system('cls')
+time.sleep(2)
+print("Welcome " + character["charname"] + " to The Tower.")
+time.sleep(2)
+print("Once you have vanquished the foes on this level, you will proceed to the one above.")
+time.sleep(2)
+print("Fight your way to the top to be crowned Champion.\nGood luck!")
+time.sleep(5)
+os.system('cls')
+time.sleep(2)
 
+#Start lvl 1
+print("TOWER LEVEL 1")
+time.sleep(2)
+
+#encounter function to pick enemies based on char level
 def encounter():
-    monster()
-    print("Look out, a " + monster.this_monster["type"] + " is attacking!")
+    if character["lvl"] < 3:
+        enemy = monster()
+    elif character["lvl"] < 5:
+        enemy = bigger_monster()
+    elif character["lvl"] < 7:
+        enemy = biggest_monster()
+
+    print("A " + enemy.this_monster["type"] + " appears!")
 
 encounter()
     
